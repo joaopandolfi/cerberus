bb88a76514cb92a9340d988bf0316c352ba43796
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2020-04-26 03:41:19 -0300

    webserver

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const navigationController = require('../controller/navigationController')
 
 router.get('/', navigationController.Home)
-router.get('/', navigationController.Test)
+router.get('/test', navigationController.Test)
 
 module.exports = router
