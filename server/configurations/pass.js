aa883e04b3952369b6a4759ee95e68569794da01
João Carlos Pandolfi <joaopandolfi@gmail.com>
2020-04-30 14:04:15 -0300

    Clean

--- server/configurations/pass.js
+++ server/configurations/pass.js
@@ -2,9 +2,9 @@ var Pass = {
       
     Socket:{
         use: true,
-        tokenPatient: "NpFedyUwUm6002ndwsA*22e!!w2r9ZcstX3AnW32qAduzGoHj",
-        tokenDoctor: "$2a$10$Eb.jEI.IDQ14B$$dakknfghb27$$pj0ed7sj6E!1NAFTcqwO6fAU1Y1B5f.3B44O",
-        tokenAdmin: "$2a$10$Eb.jEI.87wad8sad$$dakk&&&$&&nfghb27$$pj0ed7sj6E!1NAFT12399&jen%dasd}}"
+        tokenPatient: "",
+        tokenDoctor: "",
+        tokenAdmin: ""
     },
 
     session: {
